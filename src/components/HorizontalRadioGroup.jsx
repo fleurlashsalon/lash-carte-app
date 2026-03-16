095ba14c3711124816f2c44d7fe8e0c769d5487d
まさのり わたなべ <fleurlash.salon@gmail.com>
2026-03-16 14:36:07 +0900

    update icon

--- src/components/HorizontalRadioGroup.jsx
+++ src/components/HorizontalRadioGroup.jsx
@@ -27,18 +27,9 @@ export default function HorizontalRadioGroup({
                 onChange={() => onChange(opt.value)}
               />
               <span className="radioText">{opt.label}</span>
-              <span className="radioScore">{opt.value}</span>
             </label>
           )
         })}
-        <button
-          type="button"
-          className="linkButton"
-          onClick={() => onChange(undefined)}
-          aria-label={`${label}を未選択に戻す`}
-        >
-          クリア
-        </button>
       </div>
     </div>
   )
